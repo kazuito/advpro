8a49bea5890c35f5144dd582e4351f77c734abd3
Kazuma Ito <zarigani2x@gmail.com>
2024-04-18 00:47:46 +0900

    a

--- 2/QuadraticEquation.java
+++ 2/QuadraticEquation.java
@@ -1,2 +1,30 @@
 // 第2講 練習問題2-3 二次方程式の解
 // https://ksuap.github.io/2022autumn/lesson02/assignments/#3-二次方程式の解
+
+public class QuadraticEquation {
+  void run(String[] args) {
+    Double a = Double.parseDouble(args[0]);
+    Double b = Double.parseDouble(args[1]);
+    Double c = Double.parseDouble(args[2]);
+
+    Double d = b * b - 4 * a * c;
+
+    if (d > 0) {
+      Double x1 = (-b + Math.sqrt(d)) / (2 * a);
+      Double x2 = (-b - Math.sqrt(d)) / (2 * a);
+      System.out.printf("answer = %f, %f\n", x1, x2);
+    } else if (d == 0) {
+      Double x = -b / (2 * a);
+      System.out.printf("x = %f\n", x);
+    } else {
+      Double real = -b / (2 * a);
+      Double imaginary = Math.sqrt(-d) / (2 * a);
+      System.out.printf("answer = %f + %f i, %f - %f i\n", real, imaginary, real, imaginary);
+    }
+  }
+
+  public static void main(String[] args) {
+    QuadraticEquation solver = new QuadraticEquation();
+    solver.run(args);
+  }
+}
