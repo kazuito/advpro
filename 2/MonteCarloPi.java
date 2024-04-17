23c13eb4e709df4db33918fe3a315d2a2aef80bb
Kazuma Ito <zarigani2x@gmail.com>
2024-04-18 00:58:00 +0900

    a

--- 2/MonteCarloPi.java
+++ 2/MonteCarloPi.java
@@ -1,3 +1,38 @@
 // 第2講 練習問題2-4 モンテカルロ法によるπの計算
 // https://ksuap.github.io/2022autumn/lesson02/assignments/#4-モンテカルロ法によるpiの計算
 
+public class MonteCarloPi {
+  void run(String[] args) {
+    Integer n = 1000;
+
+    if (args.length > 0) {
+      n = Integer.parseInt(args[0]);
+    }
+
+    Integer hitCount = 0;
+
+    for (int i = 0; i < n; i++) {
+      if (hitOrNot()) {
+        hitCount++;
+      }
+    }
+
+    Double pi = 4.0 * hitCount / n;
+
+    System.out.printf("%f\n", pi);
+  }
+
+  Boolean hitOrNot() {
+    Double x = Math.random();
+    Double y = Math.random();
+
+    Double distance = Math.sqrt(x * x + y * y);
+
+    return distance <= 1;
+  }
+
+  public static void main(String[] args) {
+    MonteCarloPi pi = new MonteCarloPi();
+    pi.run(args);
+  }
+}
