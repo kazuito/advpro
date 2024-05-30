6227aaa85ae5edf03280acaf983087761e677a50
Kazuma Ito <zarigani2x@gmail.com>
2024-05-30 10:04:53 +0900

    a

--- 7/Gradiation2.java
+++ 7/Gradiation2.java
@@ -1,2 +1,30 @@
 // 第07講 画像操作 練習問題7-1 グラデーション2
 // https://ksuap.github.io/2022autumn/lesson07/assignments/#1-グラデーション2
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Gradiation2 {
+  void run() throws IOException {
+    BufferedImage image = createImage();
+    ImageIO.write(image, "png", new File("gradiation2.png"));
+  }
+
+  BufferedImage createImage() {
+    BufferedImage image = new BufferedImage(255, 255, BufferedImage.TYPE_INT_RGB);
+    for (Integer x = 0; x < 255; x++) {
+      for (Integer y = 0; y < 255; y++) {
+        image.setRGB(x, y, x | y << 16);
+      }
+    }
+    return image;
+  }
+
+  public static void main(String[] args) throws IOException {
+    Gradiation2 g = new Gradiation2();
+    g.run();
+  }
+}
