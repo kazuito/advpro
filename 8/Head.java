bc9efb244daf5b63510d209c78355dcaa8a99a0d
Kazuma Ito <zarigani2x@gmail.com>
2024-06-13 10:27:18 +0900

    head, tee

--- 8/Head.java
+++ 8/Head.java
@@ -1,2 +1,36 @@
 // 第８講 練習問題8-3 Headコマンドの作成
 // https://ksuap.github.io/2022autumn/lesson08/assignments/#3-head-コマンドの作成
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Head {
+
+  void run(String[] args) throws IOException {
+    Integer maxLine = Integer.parseInt(args[0]);
+    BufferedReader in;
+
+    // コマンドライン引数が1つしか与えられなかった場合．
+    if (args.length == 1) {
+      in = new BufferedReader(new InputStreamReader(System.in));
+    } else {
+      in = new BufferedReader(new FileReader(args[1]));
+    }
+
+    String line;
+
+    for (int i = 0; i < maxLine; i++) {
+      line = in.readLine();
+      System.out.println(line);
+    }
+
+    in.close();
+  }
+
+  public static void main(String[] args) throws IOException {
+    Head head = new Head();
+    head.run(args);
+  }
+}
--- 8/Tee.java
+++ 8/Tee.java
@@ -1,2 +1,32 @@
 // 第８講 練習問題8-4 Teeコマンドの作成
 // https://ksuap.github.io/2022autumn/lesson08/assignments/#4-tee-コマンドの作成
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Tee {
+  void run(String[] args) throws IOException {
+
+    String fileName = args[0];
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+
+    String line;
+
+    while ((line = in.readLine()) != null) {
+      System.out.println(line);
+      writer.println(line);
+    }
+
+    writer.close();
+  }
+
+  public static void main(String[] args) throws IOException {
+    Tee tee = new Tee();
+    tee.run(args);
+  }
+}
