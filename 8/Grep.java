60f36fc41a212e69d48a8cff1e5f4a42c9a48151
Kazuma Ito <zarigani2x@gmail.com>
2024-06-13 09:45:29 +0900

    grep

--- 8/Grep.java
+++ 8/Grep.java
@@ -1,2 +1,35 @@
 // 第８講 練習問題8-2 Grepコマンドの作成
 // https://ksuap.github.io/2022autumn/lesson08/assignments/#2-grep-コマンドの作成
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Grep {
+  void run(String[] args) throws IOException {
+    String targetStr = args[0];
+
+    for (int i = 1; i < args.length; i++) {
+      grep(new File(args[i]), targetStr);
+    }
+  }
+
+  void grep(File file, String targetStr) throws IOException {
+    BufferedReader in = new BufferedReader(new FileReader(file));
+    String line;
+
+    while ((line = in.readLine()) != null) {
+      if (line.contains(targetStr)) {
+        System.out.printf("%s: %s\n", file.getName(), line);
+      }
+    }
+
+    in.close();
+  }
+
+  public static void main(String[] args) throws IOException {
+    Grep grep = new Grep();
+    grep.run(args);
+  }
+}
